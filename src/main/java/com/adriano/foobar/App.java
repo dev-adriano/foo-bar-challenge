789a9f9c0acb31b83b486985b3d7f67922120138
Adriano <jadrian.osoriop@gmail.com>
2019-03-25 17:28:38 -0600

    adding maven project

---
+++ src/main/java/com/adriano/foobar/App.java
@@ -0,0 +1,13 @@
+package com.adriano.foobar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
